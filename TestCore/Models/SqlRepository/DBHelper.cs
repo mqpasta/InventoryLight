fe1008acd115c2ae7a7abd35dcce07f12629f983
mqpasta <mqpasta@DESKTOP-1IN5GU6.localdomain>
2021-08-08 05:44:44 +0500

    fix db password issue for docker

--- TestCore/Models/SqlRepository/DBHelper.cs
+++ TestCore/Models/SqlRepository/DBHelper.cs
@@ -14,12 +14,13 @@ namespace TestCore.Models.SqlRepository
     public class DBHelper
     {
         private static SqlConnection _connection;
-        private static string _connectionString = @"Data Source={0};Initial Catalog={1};User ID={2};Password={3}";
+        private static string _connectionString = @"Data Source={0};Initial Catalog={1};User ID={2};Password={3};Trusted_Connection=False";
 
         public static void SetConnectionString(string dbServer, string dbName,
                                                 string userId, string password)
         {
             _connectionString = string.Format(_connectionString, @dbServer, @dbName, userId, password);
+            Console.WriteLine(_connectionString);
         }
 
         public static string ConnectionString
