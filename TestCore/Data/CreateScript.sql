ec7e4c44d3c7b5bd2b36f60ea069beecc118c621
mqpasta <mqpasta@gmail.com>
2021-08-09 02:59:46 +0500

    added script for products

--- TestCore/Data/CreateScript.sql
+++ TestCore/Data/CreateScript.sql
@@ -3,7 +3,7 @@ GO
 
 USE [PastaPackages]
 GO
-/****** Object:  Table [dbo].[Location]    Script Date: 8/7/2021 8:56:29 PM ******/
+/****** Object:  Table [dbo].[Location]    Script Date: 8/9/2021 2:21:14 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -13,13 +13,14 @@ CREATE TABLE [dbo].[Location](
 	[LocationCode] [int] NOT NULL,
 	[LocationName] [nvarchar](50) NOT NULL,
 	[LastUpdated] [datetime] NOT NULL,
+	[IsWearhouse] [bit] NOT NULL,
  CONSTRAINT [PK_Location] PRIMARY KEY CLUSTERED 
 (
 	[LocationId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Product]    Script Date: 8/7/2021 8:56:29 PM ******/
+/****** Object:  Table [dbo].[Product]    Script Date: 8/9/2021 2:21:14 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -37,7 +38,27 @@ CREATE TABLE [dbo].[Product](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[StockMovement]    Script Date: 8/7/2021 8:56:29 PM ******/
+/****** Object:  Table [dbo].[PurchaseOrder]    Script Date: 8/9/2021 2:21:14 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[PurchaseOrder](
+	[PurchaseOrderId] [bigint] IDENTITY(1,1) NOT NULL,
+	[PODate] [date] NOT NULL,
+	[ProductId] [bigint] NOT NULL,
+	[ConvRate] [decimal](18, 2) NOT NULL,
+	[RMBRate] [decimal](18, 2) NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[ReceivedQuantity] [int] NOT NULL,
+	[IsReceived] [bit] NOT NULL,
+ CONSTRAINT [PK_PurchaseOrder] PRIMARY KEY CLUSTERED 
+(
+	[PurchaseOrderId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[StockMovement]    Script Date: 8/9/2021 2:21:14 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -53,13 +74,14 @@ CREATE TABLE [dbo].[StockMovement](
 	[SalePrice] [decimal](18, 2) NOT NULL,
 	[LastUpdate] [datetime] NOT NULL,
 	[StockMovementType] [int] NOT NULL,
+	[PurchaseOrderId] [bigint] NULL,
  CONSTRAINT [PK_StockMovement] PRIMARY KEY CLUSTERED 
 (
 	[StockMovementId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[StockStatus]    Script Date: 8/7/2021 8:56:29 PM ******/
+/****** Object:  Table [dbo].[StockStatus]    Script Date: 8/9/2021 2:21:14 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -77,7 +99,7 @@ CREATE TABLE [dbo].[StockStatus](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  View [dbo].[StockStatusView]    Script Date: 8/7/2021 8:56:29 PM ******/
+/****** Object:  View [dbo].[StockStatusView]    Script Date: 8/9/2021 2:21:14 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -89,13 +111,54 @@ FROM            dbo.StockStatus AS SS INNER JOIN
                          dbo.Product AS P ON SS.ProductId = P.ProductId INNER JOIN
                          dbo.Location AS L ON L.LocationId = SS.LocationId
 GO
+/****** Object:  View [dbo].[ViewPurchaseOrders]    Script Date: 8/9/2021 2:21:14 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW [dbo].[ViewPurchaseOrders]
+AS
+SELECT        PO.PurchaseOrderId, PO.PODate, PO.ProductId, PO.ConvRate, PO.RMBRate, PO.Quantity, PO.IsReceived, ISNULL(SUM(SM.Qty), 0) AS ReceivedQuantity
+FROM            dbo.PurchaseOrder AS PO LEFT OUTER JOIN
+                         dbo.StockMovement AS SM ON SM.PurchaseOrderId IS NOT NULL AND PO.PurchaseOrderId = SM.PurchaseOrderId AND PO.ProductId = SM.ProductId
+GROUP BY PO.PurchaseOrderId, PO.PODate, PO.ProductId, PO.ConvRate, PO.RMBRate, PO.Quantity, PO.IsReceived
+GO
+/****** Object:  View [dbo].[ViewWearhouseStock]    Script Date: 8/9/2021 2:21:14 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[ViewWearhouseStock]
+AS
+SELECT        dbo.Product.ProductName, SUM(dbo.StockStatus.PurchaseQty - dbo.StockStatus.SaleQty) AS BalanceQuantity
+FROM            dbo.Location INNER JOIN
+                         dbo.StockStatus ON dbo.Location.LocationId = dbo.StockStatus.LocationId INNER JOIN
+                         dbo.Product ON dbo.StockStatus.ProductId = dbo.Product.ProductId
+WHERE        (dbo.Location.IsWearhouse = 1)
+GROUP BY dbo.Product.ProductName
+GO
 ALTER TABLE [dbo].[Location] ADD  CONSTRAINT [DF_Location_LastUpdated]  DEFAULT (getdate()) FOR [LastUpdated]
+GO
+ALTER TABLE [dbo].[Location] ADD  CONSTRAINT [DF_Location_IsWearhouse]  DEFAULT ((0)) FOR [IsWearhouse]
 GO
 ALTER TABLE [dbo].[Product] ADD  CONSTRAINT [DF_Product_PurchasePrice]  DEFAULT ((0)) FOR [PurchasePrice]
 GO
 ALTER TABLE [dbo].[Product] ADD  CONSTRAINT [DF_Product_SalePrice]  DEFAULT ((0)) FOR [SalePrice]
 GO
 ALTER TABLE [dbo].[Product] ADD  CONSTRAINT [DF_Product_LastUpdate]  DEFAULT (getdate()) FOR [LastUpdate]
+GO
+ALTER TABLE [dbo].[PurchaseOrder] ADD  CONSTRAINT [DF_PurchaseOrder_PODate]  DEFAULT (getdate()) FOR [PODate]
+GO
+ALTER TABLE [dbo].[PurchaseOrder] ADD  CONSTRAINT [DF_PurchaseOrder_ConvRate]  DEFAULT ((0.0)) FOR [ConvRate]
+GO
+ALTER TABLE [dbo].[PurchaseOrder] ADD  CONSTRAINT [DF_PurchaseOrder_RMBRate]  DEFAULT ((0.0)) FOR [RMBRate]
+GO
+ALTER TABLE [dbo].[PurchaseOrder] ADD  CONSTRAINT [DF_PurchaseOrder_Quantity]  DEFAULT ((0)) FOR [Quantity]
+GO
+ALTER TABLE [dbo].[PurchaseOrder] ADD  CONSTRAINT [DF_PurchaseOrder_ReceivedQuantity]  DEFAULT ((0)) FOR [ReceivedQuantity]
+GO
+ALTER TABLE [dbo].[PurchaseOrder] ADD  CONSTRAINT [DF_PurchaseOrder_IsReceived]  DEFAULT ((0)) FOR [IsReceived]
 GO
 ALTER TABLE [dbo].[StockMovement] ADD  CONSTRAINT [DF_StockMovement_Date]  DEFAULT (getdate()) FOR [Date]
 GO
@@ -115,6 +178,11 @@ ALTER TABLE [dbo].[StockStatus] ADD  CONSTRAINT [DF_StockStatus_SaleQty]  DEFAUL
 GO
 ALTER TABLE [dbo].[StockStatus] ADD  CONSTRAINT [DF_StockStatus_LastUpdate]  DEFAULT (getdate()) FOR [LastUpdate]
 GO
+ALTER TABLE [dbo].[PurchaseOrder]  WITH CHECK ADD  CONSTRAINT [FK_PurchaseOrder_Product] FOREIGN KEY([ProductId])
+REFERENCES [dbo].[Product] ([ProductId])
+GO
+ALTER TABLE [dbo].[PurchaseOrder] CHECK CONSTRAINT [FK_PurchaseOrder_Product]
+GO
 ALTER TABLE [dbo].[StockMovement]  WITH CHECK ADD  CONSTRAINT [FK_StockMovement_Location] FOREIGN KEY([ToLocationId])
 REFERENCES [dbo].[Location] ([LocationId])
 GO
@@ -129,6 +197,11 @@ ALTER TABLE [dbo].[StockMovement]  WITH CHECK ADD  CONSTRAINT [FK_StockMovement_
 REFERENCES [dbo].[Product] ([ProductId])
 GO
 ALTER TABLE [dbo].[StockMovement] CHECK CONSTRAINT [FK_StockMovement_Product]
+GO
+ALTER TABLE [dbo].[StockMovement]  WITH CHECK ADD  CONSTRAINT [FK_StockMovement_PurchaseOrder] FOREIGN KEY([PurchaseOrderId])
+REFERENCES [dbo].[PurchaseOrder] ([PurchaseOrderId])
+GO
+ALTER TABLE [dbo].[StockMovement] CHECK CONSTRAINT [FK_StockMovement_PurchaseOrder]
 GO
 ALTER TABLE [dbo].[StockStatus]  WITH CHECK ADD  CONSTRAINT [FK_StockStatus_Location] FOREIGN KEY([LocationId])
 REFERENCES [dbo].[Location] ([LocationId])
@@ -270,4 +343,279 @@ End
 ' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'StockStatusView'
 GO
 EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'StockStatusView'
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "PO"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 199
+               Right = 269
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "SM"
+            Begin Extent = 
+               Top = 6
+               Left = 307
+               Bottom = 204
+               Right = 692
+            End
+            DisplayFlags = 280
+            TopColumn = 3
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 10
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1815
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 12
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'ViewPurchaseOrders'
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'ViewPurchaseOrders'
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "Location"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 136
+               Right = 208
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Product"
+            Begin Extent = 
+               Top = 32
+               Left = 760
+               Bottom = 162
+               Right = 930
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "StockStatus"
+            Begin Extent = 
+               Top = 6
+               Left = 454
+               Bottom = 136
+               Right = 624
+            End
+            DisplayFlags = 280
+            TopColumn = 2
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 9
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 12
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'ViewWearhouseStock'
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'ViewWearhouseStock'
 GO
