8bde031243d45146fc68f764d95954bbf95adc21
mqpasta <mqpasta@gmail.com>
2021-08-18 19:02:22 +0500

    interm changes

---
+++ TestCore/Controllers/ReportController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using TestCore.Models;
+using TestCore.Models.Reports;
+using TestCore.Models.SqlRepository;
+using TestCore.Models.IRepository;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TestCore.Controllers
+{
+    public class ReportController : Controller
+    {
+        IProductRepository _prodRep = new SqlProductRepository();
+        ILocationRepository _locRep = new SqlLocationRepository();
+        IPurchaseRepository rep = new SqlPurchaseRepository();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GoodReceive()
+        {
+
+            GoodReceiveReport r = new GoodReceiveReport();
+            r.PurchaseMovements = new List<PurchaseMovement>();
+            r.PurchaseMovements.Add(new PurchaseMovement());
+
+            SetDropDownLists(null);
+
+            return View(r);
+        }
+
+        public IActionResult GoodReceiveFilter(GoodReceiveReport filter)
+        {
+            SetDropDownLists(filter.ProductId);
+
+            return View("GoodReceive", filter);
+        }
+
+        private void SetDropDownLists(long? selectedProduct)
+        {
+
+            List<Location> locs = _locRep.GetLocations();
+            locs.Insert(0,
+                new Location()
+                {
+                    LocationId = -1,
+                    LocationName = "All"
+                });
+            ViewBag.VBLocationList = locs;
+
+            List<Product> prods = _prodRep.GetProducts() as List<Product>;
+            prods.Insert(0, new Product()
+            {
+                ProductId = -1,
+                ProductName = "All"
+            });
+
+
+            ViewBag.VBProductList = new SelectList(prods, "ProductId", "ProductCodeName", selectedProduct);
+        }
+    }
+}
---
+++ TestCore/Models/Reports/GoodReceiveReport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using TestCore.Models.IRepository;
+using TestCore.Models.SqlRepository;
+namespace TestCore.Models.Reports
+{
+    public class GoodReceiveReport
+    {
+        [DisplayName("Start Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:dd-MMM-yyyy}")]
+        public DateTime? StartDate { get; set; }
+
+        [DisplayName("End Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:dd-MMM-yyyy}")]
+        public DateTime? EndDate { get; set; }
+
+        [DisplayName("Location")]
+        public long LocationId { get; set; }
+
+        [DisplayName("Product")]
+        public long ProductId { get; set; }
+
+        public List<PurchaseMovement> PurchaseMovements { get; set; }
+    }
+}
