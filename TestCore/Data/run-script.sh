736bd5c1d32433564fb84d2cd5d21d20744fe700
mqpasta <mqpasta@gmail.com>
2021-08-21 22:19:19 +0500

    db deployment script for cost price

--- TestCore/Data/run-script.sh
+++ TestCore/Data/run-script.sh
@@ -5,3 +5,5 @@
  # /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "King.1234" -d master -i setup.sql
  # sleep 1s
  # /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "King.1234" -d master -i InsertProducts.sql
+   sleep 1s
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "King.1234" -d master -i UpdateCostPrice.sql
