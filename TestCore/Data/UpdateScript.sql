9a8fe9c0eefb8bc029041dc9862684f991715c43
mqpasta <mqpasta@gmail.com>
2021-08-22 00:26:50 +0500

    disable db scripts

--- TestCore/Data/UpdateScript.sql
+++ TestCore/Data/UpdateScript.sql
@@ -1,4 +1,4 @@
-begin tran
-update StockMovement set Date = '2021-01-01' Where StockMovementType = 2
-select top 10 * from StockMovement WHERE StockMovementType=2
-commit
+--begin tran
+--update StockMovement set Date = '2021-01-01' Where StockMovementType = 2
+--select top 10 * from StockMovement WHERE StockMovementType=2
+--commit
