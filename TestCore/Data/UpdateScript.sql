b6f6f65a9e09c29d4713e8676edb1b179f17de4a
mqpasta <mqpasta@gmail.com>
2021-08-22 00:30:31 +0500

    fixed: update script for opening stock date

--- TestCore/Data/UpdateScript.sql
+++ TestCore/Data/UpdateScript.sql
@@ -1,4 +1,7 @@
---begin tran
---update StockMovement set Date = '2021-01-01' Where StockMovementType = 2
---select top 10 * from StockMovement WHERE StockMovementType=2
---commit
+Use [PastaPackages]
+Go
+
+begin tran
+update StockMovement set Date = '2021-01-01' Where StockMovementType = 2
+select top 10 * from StockMovement WHERE StockMovementType=2
+commit
